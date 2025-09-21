1727d3e96f70d38e4a3e133d16dfbbb115830bdf
abid211051 <apranto41@gmail.com>
2025-09-22 00:41:51 +0600

    All required feature's is implemented

--- client.js
+++ client.js
@@ -2,42 +2,79 @@ const dnsResolver = "https://dns.google/resolve";
 
 let form;
 let ul;
+let domainHeading;
 
 const initialize = () => {
   form = document.getElementById("input-form");
   ul = document.getElementById("output-ul");
-  form.addEventListener("submit", searchSFP);
+  domainHeading = document.getElementById("domain-heading");
+  const urlDomain = new URLSearchParams(window.location.search).get("domain");
+  if (urlDomain && urlDomain.length !== 0) {
+    fetchData(urlDomain);
+  }
+  form.addEventListener("submit", searchInput);
 };
 
-const searchSFP = async (event) => {
+const notFoundErrorMsg = () => {
+  ul.textContent = "";
+  const li = document.createElement("li");
+  li.classList.add("spfs");
+  li.style.fontSize = "15px";
+  li.style.fontFamily = "monospace";
+  const p = document.createElement("p");
+  p.style.background = "#fefce8";
+  p.style.color = "#a16207";
+  p.textContent = "Couldn't find any SPF record for that domain";
+  li.appendChild(p);
+  ul.appendChild(li);
+};
+
+const fetchData = async (domain) => {
+  try {
+    domainHeading.textContent = domain;
+    const response = await fetch(`${dnsResolver}?name=${domain}&type=TXT`);
+    const parseResponse = await response.json();
+    if (!parseResponse.Answer) {
+      throw Error(`${domain} doesn't have any SPF records.`);
+    }
+    let spfs = "";
+    for (const ele of parseResponse?.Answer) {
+      if (ele.data.substring(0, 6) === "v=spf1") {
+        spfs = ele.data;
+      }
+    }
+    visualizeData(spfs);
+  } catch (error) {
+    notFoundErrorMsg();
+  }
+};
+
+const urlUpdate = (domain) => {
+  history.pushState({ domain }, "", `?domain=${domain}`);
+  fetchData(domain);
+};
+
+window.addEventListener("popstate", (e) => {
+  if (e.state && e.state.domain) {
+    fetchData(e.state.domain);
+  } else {
+    domainHeading.textContent = "_";
+    ul.textContent = "";
+  }
+});
+
+const searchInput = async (event) => {
   event.preventDefault();
   const domain = new FormData(event.target).get("domain");
   if (domain) {
-    try {
-      const response = await fetch(`${dnsResolver}?name=${domain}&type=TXT`);
-      const parseResponse = await response.json();
-      if (!parseResponse.Answer) {
-        throw Error(`${domain} doesn't have any SPF records.`);
-      }
-      let spfs = "";
-      for (const ele of parseResponse?.Answer) {
-        if (ele.data.substring(0, 6) === "v=spf1") {
-          spfs = ele.data;
-        }
-      }
-      visualizeData(spfs);
-    } catch (error) {
-      console.log(error);
-    }
+    urlUpdate(domain);
   }
 };
 
 const visualizeData = (spfs) => {
-  console.log(spfs);
   const redirect = [];
   const include = [];
   const ip = [];
-  let i = 6;
   let j = 6;
   let substr = "";
 
@@ -65,6 +102,12 @@ const visualizeData = (spfs) => {
     j++;
   }
   ul.textContent = "";
+  console.log(!redirect.length && !include.length && !ip.length);
+
+  if (!redirect.length && !include.length && !ip.length) {
+    notFoundErrorMsg();
+    return;
+  }
   if (redirect.length > 0) {
     redirect.forEach((val) => {
       listNodeAppending(
@@ -114,16 +157,15 @@ const listNodeAppending = (firstSpan, firstP, secondSpan, secondP) => {
   p1.appendChild(span1);
   p1.appendChild(document.createTextNode(` ${firstP.frontText} `));
   if (firstSpan.text === "Redirect" || firstSpan.text === "Include") {
-    span2.id = secondSpan;
     span2.textContent = secondSpan;
     span2.style.color = "#1e40af";
     span2.style.textDecoration = "underline";
     span2.style.textUnderlineOffset = "3px";
     span2.style.cursor = "pointer";
-    span2.addEventListener("click", searchSFP);
+    span2.addEventListener("click", () => urlUpdate(secondSpan));
   } else {
     span2.textContent = secondSpan;
-    span2.style.fontWeight = 700;
+    span2.style.fontWeight = 800;
   }
   p1.appendChild(span2);
   if (firstSpan.text === "Include") {
